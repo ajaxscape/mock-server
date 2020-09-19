18b46e56b247120c4e78ff0c1c81cee70d006e84
ben <ben@ajaxscape.com>
2020-09-19 23:19:24 +0100

    Second commit

--- index.js
+++ index.js
@@ -1,29 +1,72 @@
 require('dotenv').config()
 
+const merge = require('deepmerge')
 const express = require('express')
-const {v4: uuid} = require('uuid')
-const app = express()
+const { v4: uuid } = require('uuid')
+const bodyParser = require('body-parser')
+
 const port = process.env.PORT
+
+const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 const store = {}
 
+function getItem (model, id) {
+  const collection = store[model] || []
+  return collection.find((item) => item.id === id)
+}
+
+function setItem (model, data) {
+  if (!store[model]) {
+    store[model] = []
+  }
+  if (!data.id) {
+    data.id = uuid()
+    store[model].push(data)
+  } else {
+    const index = store[model].map(({ id }) => id).indexOf(data.id)
+    if (index === -1) {
+      throw new Error('Missing entry')
+    }
+    store[model][index] = data
+  }
+  return data
+}
+
+app.get('/:model', (req, res) => {
+  const { model } = req.params
+  if (store[model]) {
+    res.send(store[model])
+  }
+  res.status(404).send()
+})
+
 app.get('/:model/:id', (req, res) => {
   const { model, id } = req.params
-  const data = (store[model] || {})[id]
-  if (data  === undefined) {
-    res.status(404).json()
+  const data = getItem(model, id)
+  if (data === undefined) {
+    res.status(404).send()
   }
-  res.json(data)
+  res.status(200).send(data)
 })
 
 app.post('/:model', (req, res) => {
   const { model } = req.params
-  if (!store[model]) {
-    store[model] = []
-  }
-  const id = uuid()
-  store[model].push({...req.body, id})
-  res.status(201).json(store[model].find((item) => item.id === id))
+  const data = setItem(model, req.body)
+  res.status(201).send(data)
+})
+
+app.put('/:model/:id', (req, res) => {
+  const { model, id } = req.params
+  res.status(200).send(setItem(model, { ...req.body, id }))
+})
+
+app.patch('/:model/:id', (req, res) => {
+  const { model, id } = req.params
+  const data = getItem(model, id)
+  res.status(200).send(setItem(model, merge(data, req.body)))
 })
 
 app.listen(port, () => {
