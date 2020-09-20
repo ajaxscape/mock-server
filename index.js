4256bb871df5d9a3b9c3ee9076ac36b34738285c
ben <ben@ajaxscape.com>
2020-09-20 11:54:48 +0100

    Add delete

--- index.js
+++ index.js
@@ -35,9 +35,21 @@ function setItem (model, data) {
   return data
 }
 
+function deleteItem (model, id) {
+  if (!store[model]) {
+    throw new Error('Missing entry')
+  }
+  const index = store[model].map(({ id }) => id).indexOf(id)
+  if (index === -1) {
+    throw new Error('Missing entry')
+  }
+  return store[model].splice(index, 1).pop()
+}
+
 app.get('/:model', (req, res) => {
   const { model } = req.params
   if (store[model]) {
+    // get items filtered by query
     // eslint-disable-next-line eqeqeq
     res.send(store[model].filter((item) => Object.entries(req.query).every(([field, val]) => item[field] == val)))
   }
@@ -55,19 +67,30 @@ app.get('/:model/:id', (req, res) => {
 
 app.post('/:model', (req, res) => {
   const { model } = req.params
+  // create item
   const data = setItem(model, req.body)
   res.status(201).send(data)
 })
 
 app.put('/:model/:id', (req, res) => {
   const { model, id } = req.params
-  res.status(200).send(setItem(model, { ...req.body, id }))
+  // replace item
+  const data = setItem(model, { ...req.body, id })
+  res.status(200).send(data)
 })
 
 app.patch('/:model/:id', (req, res) => {
   const { model, id } = req.params
-  const data = getItem(model, id)
-  res.status(200).send(setItem(model, merge(data, req.body)))
+  // update item
+  const data = setItem(model, merge(getItem(model, id), req.body))
+  res.status(200).send(data)
+})
+
+app.delete('/:model/:id', (req, res) => {
+  const { model, id } = req.params
+  // delete item
+  const data = deleteItem(model, id)
+  res.status(200).send(data)
 })
 
 app.listen(port, () => {
